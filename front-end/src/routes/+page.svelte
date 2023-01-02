f261b54135547b5039ebc810b60c2c46ea393c17
Ed Fawcett-Taylor <edwardpaultaylor@gmail.com>
2023-01-02 09:33:50 +0000

    update main page

--- front-end/src/routes/+page.svelte
+++ front-end/src/routes/+page.svelte
@@ -1,21 +1,57 @@
-<script>
-import { onMount } from "svelte";
+<script lang="ts">
+    import type { PageData } from './$types';
+    import Cross from '$lib/Cross.svelte';
+    import Step from '$lib/Step.svelte';
 
-let data = {};
-
-onMount(async () => {
-    fetch("http://localhost:8000/workout/")
-    .then(response => response.json())
-    .then(api_data => {
-        console.log(api_data);
-        data = api_data;
-    }).catch(error => {
-    console.log(error);
-    return [];
-    });
-});
+    export let data: PageData;
 </script>
 
+<header class="l-row | c-header">
+    <button class="c-header__close-button">
+        <Cross />
+    </button>
+    <h1 class="c-header__title">{data.name}</h1>
+    <button class="c-header__save-button">save</button>
+</header>
 <main>
-	<h1>{data.sport}</h1>
+    <section>
+        <h2>Overview</h2>
+        <section>
+            <h3>Notes</h3>
+            <p>{data.notes}</p>
+        </section>
+    </section>
+    <section>
+        <h2>Steps</h2>
+        <ul>
+            {#each data.steps as step}
+            <li>
+                <Step type={step.type} duration={step.duration} />
+            </li>
+            {/each}
+        </ul>
+    </section>
+    <div>
+        <button>Add step</button>
+        <button>Add repeat</button>
+    </div>
 </main>
+
+<style lang="scss">
+    *,*::before,*::after {
+        box-sizing: border-box;
+    }
+
+    .l-row {
+        display: flex;
+    }
+
+    .c-header {
+        &__close-button {
+            padding: 0.5rem;
+        }
+        &__save-button {
+            text-transform: uppercase;
+        }
+    }
+</style>
