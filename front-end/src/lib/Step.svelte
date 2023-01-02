c8b58f9934267cae25913b12348a74327f997a01
Ed Fawcett-Taylor <edwardpaultaylor@gmail.com>
2023-01-02 09:33:29 +0000

    define `Step` component

---
+++ front-end/src/lib/Step.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+    export let type: string
+    export let duration: number[];
+</script>
+
+<section>
+    <h3>{type}</h3>
+    <p>{duration}</p>
+</section>
+<button>Move</button>
+<button>Delete</button>
